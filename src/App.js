d2788091f916f04139adcc6d040089ce705aba35
Ivan Reinaldo <reinaldoivan777@gmail.com>
2022-08-24 14:38:22 +0700

    sentry react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const throwError = () => {
+    throw new Error(
+      `this is error from sentry test ${process.env.REACT_APP_ENV}`
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={throwError}>Throw Error</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const package_json = require("../package.json");
+
+Sentry.init({
+  dsn: "https://5f23df8313864cb190cbfeb39d63d7d5@o1365592.ingest.sentry.io/6661646",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+  environment: process.env.REACT_APP_ENV,
+  release: package_json.version,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
